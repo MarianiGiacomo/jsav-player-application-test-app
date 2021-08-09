031be0ae95540381d58cb27362faf8ba21f8dc8f
Giacomo Mariani <giacomo.mariani@aalto.fi>
2021-08-09 11:37:28 +0200

    updated project description

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,18 +5,25 @@ const Home = () => (
   <div>
 		<h1>About this project</h1>
 		<p>
-			This is an application to test the recording of traces from <a href="http://jsav.io/" target="_blank" rel="noopener noreferrer">JSAV </a>algorithm animation exercises,
-			and replaying the submission step-by-step starting from the recorded trace. 
-			The trace is recorded in the JAAL 1.0 format
+			This is an application to test the recording of traces from <a href="http://jsav.io/" target="_blank" rel="noopener noreferrer">JSAV </a> visual algorith simulation exercises,
+			and replaying the submission step-by-step starting from the recorded trace.
 		</p>
-    <ol>
-      <li>The <a href="https://github.com/MarianiGiacomo/jsav-exercise-recorder" target="_blank" rel="noopener noreferrer">Exercise Recorder</a> records the exercise while you are doing it.
-      </li>
-      <li>The trace is recorded and stored in the <a href="https://github.com/atilante/JAAL" target="_blank" rel="noopener noreferrer">JSON-based Algorithm Animation Language (JAAL) 1.0</a> format.
-      </li>
-      <li>The <a href="https://github.com/MarianiGiacomo/jsav-exercise-player" target="_blank" rel="noopener noreferrer">Exercise Player</a> creates the animation based on the received JAAL file
-      </li>
-  	</ol>
+		<p>
+			This application represents a Learning Management System, which loads the JSAV visual algorithm simulation exercises as third party tools from an external service.
+			In our case, the external service is a <a href="https://github.com/MarianiGiacomo/jsav-player-application-test-server" target="_blank" rel="noopener noreferrer">test server</a>.
+			The external service provides the exercises with embedded the <a href="https://github.com/MarianiGiacomo/jsav-exercise-recorder" target="_blank" rel="noopener noreferrer">Exercise Recorder</a> module,
+			which records in the <a href="https://github.com/atilante/JAAL" target="_blank" rel="noopener noreferrer">JAAL 1.0</a> format the actions performed when solving the exercise. 
+			The Exercise Recorcer posts the JAAL trace to the host window or to the specified URL.
+		</p>
+		<p>
+			The <a href="https://github.com/MarianiGiacomo/jsav-exercise-player" target="_blank" rel="noopener noreferrer">Exercise Player</a> is an application that given a JAAL 1.0 file, 
+			allows to replay the recorded exercise trace and exercise model answer (if given in the JAAL file).
+			This application, simulating a Learning Management System, loads the Exercise Player as external tool, in our case from the test server. 
+			The Exercise Player is loaded right after the submission of the JSAV visual algorithm simulation exercise.
+		</p>
+		<p>
+			On top of solving and replaying JSAV visual algorithm exercises, you can also see the content of the JAAL file from your last submission using the <Link to="/jaal">JAAL Visualizer</Link>.
+		</p>
 		<p>
 			Start by trying out some <Link to="/exercises">exercise</Link>. After submitting an exercise, you will see the <a href="https://github.com/MarianiGiacomo/jsav-exercise-player" target="_blank" rel="noopener noreferrer">Exercise Player</a>,
 			where you can review your submission and compare it with the model answer. Both, the submission steps and the model answer, are reconstructed from the JAAL file. 
